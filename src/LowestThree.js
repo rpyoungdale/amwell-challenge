237c6bce62de5d38f04c126e1b58767ea790d686
rpyoungdale <youngdale95@gmail.com>
2020-05-31 20:41:28 -0400

    better variable names

--- src/LowestThree.js
+++ src/LowestThree.js
@@ -9,25 +9,25 @@ export default function LowestThree() {
       const res = await fetch('https://cors-anywhere.herokuapp.com/https://www.iwillfearnoevil.com/screen/string.txt');
       if(res.ok) {
         const text = await res.text();
-        const unsortedDataArray = text.split(/\r?\n/);
+        const rawDataArray = text.split(/\r?\n/);
   
         // Maps and filters the data simultaneously. If !isNaN(parseInt(data))
         // evaluates to true, that indicates that the element does not contain
-        // letters. If the current element des not contain letters, it is kept
+        // letters. If the current element does not contain letters, it is kept
         // and added to the dataElements array as an integer. If it does contain
         // letters, it is not added to the array.
-        let dataElements = unsortedDataArray.flatMap((data) => {
-            return !isNaN(parseInt(data)) ? [parseInt(data)] : [];
+        let manipulatedData = rawDataArray.flatMap((elem) => {
+            return !isNaN(parseInt(elem)) ? [parseInt(elem)] : [];
         })
   
-        dataElements.sort((a, b) => a - b);
+        manipulatedData.sort((a, b) => a - b);
   
         // Creates a new Set with the array which removes the duplicates
         // because sets can only have unique values. The spread operator is
         // then used to convert the unique values back into an array.
-        dataElements = [...new Set(dataElements)];
+        manipulatedData = [...new Set(manipulatedData)];
   
-        setLowestThree(dataElements.slice(0, 3));
+        setLowestThree(manipulatedData.slice(0, 3));
       } else {
         throw new Error('Failed to get the data!');
       }
